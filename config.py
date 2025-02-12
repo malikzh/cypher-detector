b2cd5cf0f8863bd0aa070e765c211aa4cdeb45fa
Malik Z <cmalikz.h@gmail.com>
2025-02-12 19:42:43 +0500

    fix: epochs

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ def get_configuration():
         'BATCH_SIZE': 128,
         'LEARNING_RATE': 0.001,
         'WEIGHT_DECAY': 0.001,
-        'EPOCHS': 10,
+        'EPOCHS': 25,
         'DEVICE': 'cuda' if torch.cuda.is_available() else 'cpu',
         'BYTES_QUANTITY': 256
     }
