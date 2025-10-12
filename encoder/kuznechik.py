82dfd4fa9abb64a3f47271dfe76746c7104be5ff
Malik Z. <cmalikz.h@gmail.com>
2025-10-12 14:32:56 +0500

    feat: kuznechik + 3des

---
+++ encoder/kuznechik.py
@@ -0,0 +1,15 @@
+from . import Encoder
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+import gostcrypto
+
+class KuznechikEncoder(Encoder):
+    def encrypt(self, text: bytes, key: bytes) -> bytes:
+        cipher_obj = gostcrypto.gostcipher.new('kuznechik',
+                                        key,
+                                        gostcrypto.gostcipher.MODE_CBC,
+                                        pad_mode=gostcrypto.gostcipher.PAD_MODE_1)
+
+        return b"" + cipher_obj.encrypt(text)
---
+++ encoder/triple_des.py
@@ -0,0 +1,28 @@
+from . import Encoder
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+
+class TripleDesEncoder(Encoder):
+    def __init__(self):
+        self.backend = default_backend()
+
+    def encrypt(self, text: bytes, key: bytes) -> bytes:
+        """Шифрование 3DES в режиме CBC"""
+        # Генерация IV (8 байт для 3DES)
+        iv = os.urandom(8)
+        
+        # Создание cipher
+        cipher = Cipher(algorithms.TripleDES(key), modes.CBC(iv), backend=self.backend)
+        encryptor = cipher.encryptor()
+        
+        # Padding
+        padder = padding.PKCS7(64).padder()
+        padded_data = padder.update(text) + padder.finalize()
+        
+        # Шифрование
+        ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+        
+        # Возвращаем IV + зашифрованные данные
+        return ciphertext
